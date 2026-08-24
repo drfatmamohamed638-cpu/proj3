f9a14671ef57f129f6bb18bdfe31bb54ba11b103
fatma <drfatmamohamed60@gmail.com>
2026-08-24 23:30:12 +0300

    if condition

--- index.py
+++ index.py
@@ -1,3 +1,53 @@
 print("fatma")
 print ("welcome")
 print("my changes to proj3")
+
+
+
+# comments math operations
+x = 3
+y = 2 
+print (x + y)
+print (x - y)
+print (x * y)
+n = (x / y)
+print(n)
+
+
+
+# take data from user
+# username = input("what is your name?")
+# print ("hello!", username)
+
+
+
+
+# convert string to int
+# age = int (input ("how old are you? ") )
+# print (int(age))
+
+
+# if condition
+# success = 50
+# success = int(input("enter your degree "))
+
+# if success >= 100:
+#     print("you are success")
+# else:
+#     print("failure")
+
+
+
+# age = int (input("how old are you?" ) )
+# if age >= 20:
+#    print ("you can open")
+# else:
+#     print ("you can not open")
+
+
+
+weight =int (input("what is your weight?" ) )
+if weight < 80:
+    print("the best weight")
+else:
+    print ("you must lost your weight")
