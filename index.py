225c969286313b21955f179adec89bf9fbb13412
fatma <drfatmamohamed60@gmail.com>
2026-08-30 00:34:48 +0300

    task1

--- index.py
+++ index.py
@@ -1,17 +1,17 @@
-print("fatma")
-print ("welcome")
-print("my changes to proj3")
+# print("fatma")
+# print ("welcome")
+# print("my changes to proj3")
 
 
 
 # comments math operations
-x = 3
-y = 2 
-print (x + y)
-print (x - y)
-print (x * y)
-n = (x / y)
-print(n)
+# x = 3
+# y = 2 
+# print (x + y)
+# print (x - y)
+# print (x * y)
+# n = (x / y)
+# print(n)
 
 
 
@@ -54,9 +54,59 @@ print(n)
 
 
 
-# if condition
-x = int (input("what is x?" ) )
-if x > 0:
-      print("the x is positive")
-else: 
-      print("the x is negative")
+#  if condition
+# x = int (input("what is x?" ) )
+# if x > 0:
+#       print("the x is positive")
+# else: 
+#       print("the x is negative")
+      
+# even and odd
+# num = int(input("enter a number" ) )
+# if num % 2 == 0:
+#       print("even")
+# else:
+#       print("odd")
+
+
+# num = int(input("enter a number" ) )
+# if num >= 90:
+#       print("excelant")
+# elif num >= 70:
+#       print("very good")
+# elif num >= 60:
+#       print("good")
+# else:
+#       print("fail")
+
+
+# print("1")
+# print("2") 
+# print("3")
+# print("4")
+# print("5")
+
+# for i in range(5):
+#       print("hi")
+      
+      
+# for i in "fatma":
+#       print(i)
+      
+      
+# students = ["ali0","fatma","shahd"]
+# for i in students:
+#       print (i)
+      
+      
+# birthday =[22 , 2 , 2002]
+# for i in birthday: 
+#       print (i)
+
+
+
+ for i in range (1, 20):
+      if i % 2 == 0:
+           print ("even")
+     else: 
+           print ("odd")
---
+++ test.py
@@ -0,0 +1,13 @@
+
+# for i in range (1, 20):
+#        if i % 2 == 0:
+#             print ("even")
+#        else: 
+#             print ("odd")
+           
+           
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(f"{i} is even")
+#     else:
+#         print(f"{i} is odd")          
