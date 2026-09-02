86145e202fb017a7abb1793104437d49d926d3ac
fatma <drfatmamohamed60@gmail.com>
2026-09-03 00:09:23 +0300

    operation

--- test.py
+++ test.py
@@ -5,9 +5,139 @@
 #        else: 
 #             print ("odd")
            
-           
+'''
+hgbhghgghbhg
+'''
+
 # for i in range(1, 21):
 #     if i % 2 == 0:
 #         print(f"{i} is even")
 #     else:
 #         print(f"{i} is odd")          
+
+
+# x = y = z = 8
+# print (x)
+# print (y)
+# print (z)
+
+
+# data type
+# a = 10
+# print (type(a) )
+ 
+# x = str(20)
+# print (x)
+
+# print (type(x) )
+
+# x = float(20)
+# print (x)
+
+
+
+# str = "fatma mohamed"
+# print (str[0])
+
+# concatination 
+# name = "fatma "
+# parent = "mohamed"
+# print (name + parent)
+ 
+# print (name*4)
+
+# a = 5
+# b = 4
+# if a > b:
+#     print ("b greatet than a")
+# else:
+#     print ("a greater than b")
+
+
+# a =int (input("enter a"))
+# b =int (input("enter b"))
+# c =int (input("enter c"))
+# if a>b or b>c:
+#     print ("you can vote")
+# else:
+#    print ("sorry you cannot vote")    
+
+i = 1
+# while i <= 5:
+#     print (i);
+#     i+=1;
+# else:
+#     print("while loop is ended")
+
+# while i <=20:
+#     if i %2==0:
+#         # break
+#         continue
+#     else: 
+#         print (i);
+#         i+=1;
+    
+    
+# for i in range (50):
+#     if i %2==0:
+#         continue
+#     print (i)
+#     i+=1
+    
+    
+# current_year = 2026
+# current_month = 8
+# current_day = 31
+# birth_year = 1999
+# birth_month = 12
+# birth_day = 1
+# age = current_year - birth_year 
+# month = current_month - birth_month
+# day = current_day - birth_day
+# print(age, month, day)
+# 
+# 
+print ("choose operation")
+num1 = float(input("num1" ))
+operation = input("choose operation(+ , -, *, /):")
+num2 = float(input("num2" )) 
+if operation == '+':
+    print (num1 + num2)
+elif operation == '-':
+    print  (num1 - num2)   
+elif operation == '*':
+    print (num1 * num2)
+elif operation == '/':
+    if num2!=0:
+        print(num1 / num2)
+    else:
+        print("لا تقبل القسمه علي صفر")
+else:
+    print("عملية غير صالحه")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
